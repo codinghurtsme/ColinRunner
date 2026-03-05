3d9155bb7495a6965e9937a135fa498725127333
Colin Bauer <badgerfantoo@gmail.com>
2026-03-04 19:01:01 -0600

    Coordinates - New Coordinate System- 3-4-2026

--- TeamCode/src/main/java/PID/Coordinates.java
+++ TeamCode/src/main/java/PID/Coordinates.java
@@ -1,4 +1,40 @@
 package PID;
 
-public class Coordinates {
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+public class Coordinates{
+
+    private double x;
+    private double y;
+    protected final DistanceUnit distanceUnit;
+    private double heading;
+    protected final AngleUnit headingUnit;
+
+
+    public Coordinates(DistanceUnit distanceUnit,double x, double y, AngleUnit headingUnit, double heading) {
+        this.x = x;
+        this.y = y;
+        this.distanceUnit = distanceUnit;
+        this.heading = heading;
+        this.headingUnit = headingUnit;
+    }
+    public Coordinates(double x, double y, double heading){
+        distanceUnit = DistanceUnit.INCH;
+        headingUnit = AngleUnit.RADIANS;
+        this.x = x;
+        this.y = y;
+        this.heading = heading;
+
+    }
+
+    public Pose2D toPose2D(){
+        double newX = y-72;
+        double newY = x-72;
+        double newHeading = heading + Math.PI/2;
+        return new Pose2D(distanceUnit,newX,newY,headingUnit,newHeading);
+    }
+
 }
